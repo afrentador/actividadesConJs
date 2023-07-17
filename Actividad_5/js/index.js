d81b359498274ced87e7e52154cc46480db49647
Victor Mendoza <83458033+afrentador@users.noreply.github.com>
2023-07-17 07:11:35 -0500

    Actividad 5 culminada

--- Actividad_5/js/index.js
+++ Actividad_5/js/index.js
@@ -1,10 +1,25 @@
 window.onload=inicio;
+let rotacion=0;
+let sumar=true;
 
 function inicio(){
     document.querySelector(".boton").onclick=girar;
+    actualizar();
 }
 
 function girar(){
-    document.querySelector(".flecha").style.transform="rotate(20deg)";
-}
+    if(sumar){
+        rotacion+=20;
+    }else{
+        rotacion-=20;
+    }
 
+    if(rotacion<=0 || rotacion>=360){
+        sumar=!sumar;
+    }
+    document.querySelector(".flecha").style.transform=`rotate(${rotacion}deg)`;
+    actualizar();
+}
+function actualizar(){
+    document.querySelector(".porcentaje").innerHTML=`${rotacion}º`;
+}
